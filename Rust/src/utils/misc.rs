bcb89367f8634eb5a83a6c11081c8f3ad19c8b2a
LovecraftianHorror <LovecraftianHorror@pm.me>
2018-07-16 21:29:50 -0400

    Added Test to sum_range

--- Rust/src/utils/misc.rs
+++ Rust/src/utils/misc.rs
@@ -7,3 +7,17 @@ pub fn sum_range(upper: usize) -> usize {
 
     sum
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        for i in 1..100 {
+            let known_sum = (0..=i).into_iter().sum();
+            assert_eq!(sum_range(i), known_sum);
+        }
+    }
+}
+
